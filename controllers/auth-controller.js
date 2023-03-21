ad2a1317df55e04469a00bacacef19564e6450be
Wichapon V <wichapon@gmail.com>
2023-03-21 20:24:53 +0700

    Add UserSignup and UserLogin functions to auth-controller.js

---
+++ controllers/auth-controller.js
@@ -0,0 +1,16 @@
+//for fetching data of sign up button
+function UserSignup(req,res){ 
+
+}
+
+function UserLogin(req,res){ 
+
+}
+
+
+
+
+module.exports= {
+    UserSignup:UserSignup,
+    UserLogin:UserLogin
+};
