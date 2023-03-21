0ba9c21f7e366eb546ba40b9b7c73bcb4fbde4bb
Wichapon V <wichapon@gmail.com>
2023-03-21 20:25:05 +0700

    Add auth routes and controller functions

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+const path = require('path')
 const express = require('express');
-
+const authRoutes = require('./routes/auth-route')
 const app = express();
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'))
+
+app.use(authRoutes); // add middleware for incoming request
 
 app.listen(3000);
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,7 +1,9 @@
 const express = require('express');  //make express available in this fil
-
 const router = express.Router();
+const authController = require('../controllers/auth-controller')//import function in controller
 
-router.get('/signup',);
+router.get('/signup',authController.UserSignup);
+
+router.get('/login',authController.UserLogin);
 
 module.exports = router;
