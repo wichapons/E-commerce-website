8931407b49a4b56778d953a3735ad9b024b8a4dc
Wichapon V <wichapon@gmail.com>
2023-03-23 23:25:13 +0700

    Add database connection and server listener

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const authRoutes = require('./routes/auth-route')
 const app = express();
 
+//database
+const db = require('./database/database')
+
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
@@ -10,4 +13,11 @@ app.use(express.static('public')); // makes its content available to the client 
 
 app.use(authRoutes); // add middleware for incoming request
 
-app.listen(3000);
+db.connectDb().then(()=>{
+    app.listen(3000);
+    console.log('server is now running on http://localhost:3000/'); 
+}).catch((err)=>{   //if error show log below
+    console.log('cannot connect to the database');
+    console.log(err);
+})
+
