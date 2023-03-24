be96f6fd0f9a76e10e0bedeace7f059643d1faa5
Wichapon V <wichapon@gmail.com>
2023-03-25 01:14:34 +0700

    Add CSRF protection and error handling middleware

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const path = require('path')
 const express = require('express');
 const authRoutes = require('./routes/auth-route')
 const app = express();
-
-//database
+const errorHandleMiddleware = require('./middlewares/error-handle')
+const csrf = require('csurf');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token')
 const db = require('./database/database')
+
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
 app.use(express.static('public')); // makes its content available to the client side of the application through HTTP requests.
 app.use(express.urlencoded({extended:false})); // sets up middleware in an Express application to handle URL-encoded form data.
+
+app.use(csrf()); // put CSRF protection middleware before any routes that handle user input or requests
+app.use(addCsrfTokenMiddleware);
+
+
 app.use(authRoutes); // add middleware for incoming request
+app.use(errorHandleMiddleware);
 
 db.connectDb().then(()=>{
     app.listen(3000);
