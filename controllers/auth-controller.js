371ac677666210eb0a075366dca27193a27d9e73
Wichapon V <wichapon@gmail.com>
2023-03-24 14:38:31 +0700

    Add User model and signup logic to auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,13 +1,28 @@
+const User = require('../models/user-model')
 
 function UserSignup(req,res){  //for fetching data of sign up button
     res.render('customer/authentication/signup');
 }
 
-function signup(req,res){
+async function signup(req,res){ 
+    const email = req.body.email;
+    const password = req.body.password;
+    const fullname = req.body.fullname;
+    const address = req.body.address;
+    const street = req.body.street;
+    const postal = req.body.postal;
+    const city = req.body.city;
+
+    const user = new User(email,password,fullname,address,street,postal,city);
+    await user.signup().catch((err)=>{
+        console.log(err);
+    });
+    res.redirect('/login');
 }
 
-function UserLogin(req,res){ 
 
+function UserLogin(req,res){ 
+    res.render('customer/authentication/login')
 }
 
 
