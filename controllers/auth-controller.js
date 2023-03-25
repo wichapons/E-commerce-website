5ca0f229eedf20c984e71e98b59a94307398c59d
Wichapon V <wichapon@gmail.com>
2023-03-25 23:33:29 +0700

    Add function create session/cookie when user login successfully

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,18 +1,12 @@
 const User = require('../models/user-model')
+const authUtility = require('../utility/authentication')
 
 function UserSignup(req,res){  //for fetching data of sign up button
     res.render('customer/authentication/signup');
 }
 
 async function signup(req,res){ 
-    const email = req.body.email;
-    const password = req.body.password;
-    const fullname = req.body.fullname;
-    const address = req.body.address;
-    const street = req.body.street;
-    const postal = req.body.postal;
-    const city = req.body.city;
-
+    const { email, password, fullname, address, street, postal, city } = req.body;  //get email password and so on from req.body
     const user = new User(email,password,fullname,address,street,postal,city);
     await user.signup().catch((err)=>{
         console.log(err);
@@ -20,11 +14,28 @@ async function signup(req,res){
     res.redirect('/login');
 }
 
-
 function UserLogin(req,res){ 
     res.render('customer/authentication/login')
 }
 
+async function login(req,res){
+    const {email,password} = req.body;
+    const user = new User(email,password);
+    const existingUser = await user.getUserDataByEmail();
+    if(!existingUser){
+        alert('user not found')
+        return res.redirect('/login')
+    }
+    const IsPasswordCorrect = user.verifyPassword(existingUser.password);
+    if (!IsPasswordCorrect){
+        alert('password is not correct, please try again')
+        return res.redirect('/login')
+    }
+    //after user is authorized create a session
+    authUtility.createUserSession(req,existingUser,()=>{
+        res.redirect('/')
+    });
+}
 
 
 
