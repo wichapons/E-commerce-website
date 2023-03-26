997cc1cb9152692057372bdb774288e9c4ec8ee2
Wichapon V <wichapon@gmail.com>
2023-03-26 09:54:46 +0700

    Update MongoDB collection for session storage and decrease cookie expiration time

--- config/session.js
+++ config/session.js
@@ -1,13 +1,13 @@
 const expressSession = require('express-session');
 const mongoDbSessionStore = require('connect-mongodb-session');
 
-function createSesstionOnMongoDb(){
+function createSessionOnMongoDb(){
     const  MongoDbSessionStore = mongoDbSessionStore(expressSession);
     //save session in Topazio-Online-Shop database
     const storeSession = new MongoDbSessionStore({
         uri:'mongodb://127.0.0.1:27017',
         databaseName: 'Topazio-Online-Shop',
-        collection:'sessions'
+        collection:'sessions2'
     });
 
     return storeSession;
@@ -15,12 +15,12 @@ function createSesstionOnMongoDb(){
 
 function createSessionConfig(){
     return{
-        secret: 'A{LFJKopsdgjo-3I590-3563420-5IJ4U3-0OTJKMEW[L21+65HBH',  //secret key used for signing and verifying session cookies.
+        secret: 'A+65HBH',  //secret key used for signing and verifying session cookies.
         resave:false, //This is a boolean value that indicates whether the session data should be saved to the session store even if the session was not modified during the request. If resave is set to false, the session data will only be saved to the session store if it was modified during the request. This can help to reduce the load on the session store.
         saveUninitialized:false, //indicates whether a session should be created even if it is not modified during the request.
-        storeSession: createSesstionOnMongoDb(),
+        store: createSessionOnMongoDb(),
         cookie:{
-            maxAge: 60*100*1000// cookie expires in 100mins 
+            maxAge: 60*10*1000// cookie expires in 10mins 
         }
     };
 }
