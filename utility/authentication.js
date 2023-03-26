870cb551c3859dc3b30f4beff61565ddd939d713
Wichapon V <wichapon@gmail.com>
2023-03-26 09:58:20 +0700

    Update createUserSession to use asynchronous calls

--- utility/authentication.js
+++ utility/authentication.js
@@ -1,8 +1,13 @@
-function createUserSession(req,user,action){
+async function createUserSession(req,user,action){
     req.session.userID = user._id.toString();  //_id from modgodb 
-    req.session.save(action);
-}
+    console.log('userID = '+req.session.userID);
+    await req.session.save();
+    action();
+};
 
 module.exports={
     createUserSession:createUserSession
-}
+};
+
+
+
