a2ab55b834cbe7be32df6c7f743a00b9b6e91f3b
Wichapon V <wichapon@gmail.com>
2023-03-26 20:58:55 +0700

    Commit message: Add input validation and handle errors in signup and login processes

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,16 +1,41 @@
 const User = require('../models/user-model')
-const authUtility = require('../utility/authentication')
+const authUtility = require('../utility/authentication');
+const inputValidation = require('../utility/input-validation') 
 
 function UserSignup(req,res){  //for fetching data of sign up button
     res.render('customer/authentication/signup');
 }
 
-async function signup(req,res){ 
-    const { email, password, fullname, address, street, postal, city } = req.body;  //get email password and so on from req.body
+async function signup(req,res,next){ 
+    const { email,confirmEmail, password, fullname, address, street, postal, city } = req.body;  //get email password and so on from req.body
     const user = new User(email,password,fullname,address,street,postal,city);
-    await user.signup().catch((err)=>{
-        console.log(err);
-    });
+
+    if (!inputValidation.isInputValid(email, password, fullname, address, street, postal, city)){
+        console.log('Input is invalid');
+        res.redirect('/signup')
+        return;
+    }
+
+    if(!inputValidation.isSameConfirmEmail(email,confirmEmail)){
+        console.log('Email or confirm email does not match');
+        res.redirect('/signup')
+        return;
+    }
+    
+    let getMatchedEmailDB = await user.getUserDataByEmail();
+    let existEmail = getMatchedEmailDB.email
+    try{
+
+        if(existEmail=== email){
+            console.log('This email already existed in our server');
+            res.redirect('/signup')
+            return;
+        }
+        await user.signup();
+    } catch(err){
+        console.log('cannot execute user.signup() Error:' +err);
+        return;
+    };
     res.redirect('/login');
 }
 
@@ -21,7 +46,13 @@ function UserLogin(req,res){
 async function login(req,res){
     const {email,password} = req.body;
     const user = new User(email,password);
-    const existingUser = await user.getUserDataByEmail();
+    let existingUser;
+    try{
+        existingUser = await user.getUserDataByEmail();
+    }catch(err){
+        console.log('cannot get the data of existingUser. Error:' + err);
+    }
+
     if(!existingUser){
         console.log('user not found in database');
         return res.redirect('/login');
