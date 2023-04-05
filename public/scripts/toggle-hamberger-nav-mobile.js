3440977faa7449306dce6201aba6d4bb650ee5c1
Wichapon V <wichapon@gmail.com>
2023-04-05 23:12:52 +0700

    Add event listener to mobile menu button to toggle mobile menu

--- public/scripts/toggle-hamberger-nav-mobile.js
+++ public/scripts/toggle-hamberger-nav-mobile.js
@@ -1,1 +1,8 @@
-const hamberger
+const mobileMenuButtonElement = document.getElementById('mobile-menu-btn');
+const mobileMenuElement = document.getElementById('mobile-menu');
+
+function toggleMobileMenu(){
+    mobileMenuElement.classList.toggle('open')  // add if does not exist and remove if already existed
+}
+
+mobileMenuButtonElement.addEventListener('click',toggleMobileMenu);
