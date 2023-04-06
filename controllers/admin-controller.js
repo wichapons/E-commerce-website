9279f29d67945008dfcdcff84addbf453c586849
Wichapon V <wichapon@gmail.com>
2023-04-06 22:09:17 +0700

    Add admin controller with getAllProducts, getNewProduct, and createNewProduct functions

---
+++ controllers/admin-controller.js
@@ -0,0 +1,18 @@
+function getAllProducts(req,res){
+    console.log('get all has been called');
+    res.render('admin/products/all-products');
+}
+
+function getNewProduct(req,res){
+    res.render('admin/products/new-product')
+}
+
+function createNewProduct(req,res){
+    res.render('admin/products/new-product')
+}
+
+module.exports= {
+    getNewProduct:getNewProduct,
+    getAllProducts:getAllProducts,
+    createNewProduct:createNewProduct
+}
