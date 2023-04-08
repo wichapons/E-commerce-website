0b5e6043ad9e7103dc9e0c19f3b20eb3c92d8a33
Wichapon V <wichapon@gmail.com>
2023-04-09 01:43:06 +0700

    Add image upload middleware to add products route

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,8 +1,11 @@
-const express = require('express');  //make express available in this fil
+const express = require('express');  //make express available in this file
 const adminController = require('../controllers/admin-controller');
+const imageUploadMiddleware = require('../middlewares/file-upload')
+
 const router = express.Router();
 
 router.get('/products',adminController.getAllProducts);
-router.get('/products/add',adminController.createNewProduct);
+router.get('/products/add',adminController.getNewProduct);
+router.post('/addproducts',imageUploadMiddleware,adminController.createNewProduct);
 
 module.exports = router;
