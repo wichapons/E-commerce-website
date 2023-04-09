dfe8d335c80a4b730c8bf544546dfe5c14d42ba1
Wichapon V <wichapon@gmail.com>
2023-04-09 16:12:19 +0700

    Add createNewProduct function for administrator

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,3 +1,5 @@
+const Product = require('../models/products-model')
+
 function getAllProducts(req,res){
     console.log('get all has been called');
     res.render('admin/products/all-products');
@@ -7,10 +9,21 @@ function getNewProduct(req,res){
     res.render('admin/products/new-product')
 }
 
-function createNewProduct(req,res){
+async function createNewProduct(req,res){
+    console.log(req.body);
+
+    const product = new Product({
+        ...req.body, //get only the text field in the form (file type is excluded)
+        productImageName: req.file.filename
+    });
     
-
-
+    try{
+        await product.save();
+    } catch(err){
+        console.log(`cannot save the product data to the database err:${err}`);
+        next(err);
+        return;
+    }
     res.redirect('/admin/products')
 }
 
