58d3d4db5916fe873e32fae7088e6e2faa95f84f
Wichapon V <wichapon@gmail.com>
2023-04-09 22:58:07 +0700

    Add verification for productData._id if it exits or not before create a Product

--- models/products-model.js
+++ models/products-model.js
@@ -3,14 +3,18 @@ const db = require('../database/database')
 class Product{
     constructor(productData){
         //Product details
-        this.title = productData.title,
-        this.summary = productData.productSummary,
-        this.price = parseFloat(productData.productPrice), //convert to number
-        this.description = productData.productDescription
+        this.title = productData.title;
+        this.summary = productData.summary;
+        this.price = parseFloat(productData.price); //convert to number
+        this.description = productData.description;
         //Product images
-        this.imageName = productData.productImageName,
-        this.productImagePath = `/product-data/image/${productData.imageName}`,
+        this.imageName = productData.imageName;
+        this.imagePath = `/product-data/image/${productData.imageName}`;
         this.imageUrl = `/products/image/${productData.imageName}`;
+        if(productData._id){
+            this.id = productData._id.toString();
+        }   
+        
     }
 
     async save(){
@@ -23,7 +27,16 @@ class Product{
             description: this.description,
             imageName: this.imageName
         });
+    }
 
+    static async findAll(){
+        const allProducts = await db.getDb().collection('products').find().toArray();
+        console.log('at findAll stage: ');
+        console.log(Object.values(allProducts));
+
+        return allProducts.map((productDoc)=>{
+            return new Product(productDoc);
+        });
     }
 }
 
