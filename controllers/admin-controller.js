c4fd2a797669caea1260a710da2b621e9e4ade55
Wichapon V <wichapon@gmail.com>
2023-04-09 23:02:30 +0700

    Add getAllProducts function for render all products

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,20 +1,27 @@
 const Product = require('../models/products-model')
 
-function getAllProducts(req,res){
-    console.log('get all has been called');
-    res.render('admin/products/all-products');
-}
+async function getAllProducts(req,res,next){
+    try{
+        const allProducts = await Product.findAll()
+        console.log('at getAllProducts');
+        console.log(Object.values(allProducts));
+        res.render('admin/products/all-products',{products:allProducts});
+    } catch(err){
+        console.log(`cannot get product data from the database. Error Msg: ${err}`);
+        next(err); //send to error handler middleware
+        return;
+    }
 
+}
 function getNewProduct(req,res){
     res.render('admin/products/new-product')
 }
 
 async function createNewProduct(req,res){
     console.log(req.body);
-
     const product = new Product({
         ...req.body, //get only the text field in the form (file type is excluded)
-        productImageName: req.file.filename
+        imageName: req.file.filename
     });
     
     try{
