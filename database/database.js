490970336b986d4a44c06d2f62d5fc5d518dffc0
Wichapon V <wichapon@gmail.com>
2023-04-09 16:13:15 +0700

    declare database as a global scope

--- database/database.js
+++ database/database.js
@@ -2,6 +2,7 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
+let database;
 async function connectDb(){
     const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
     database = client.db('Topazio-Online-Shop');
