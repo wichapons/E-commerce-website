6301e335670db21cbdaa8537a15ebc7876eb1776
Wichapon V <wichapon@gmail.com>
2023-04-14 14:25:36 +0700

    Add deleteProduct route

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -4,10 +4,11 @@ const imageUploadMiddleware = require('../middlewares/file-upload')
 
 const router = express.Router();
 
-router.get('/products',adminController.getAllProducts);
-router.get('/products/add',adminController.getNewProduct);
-router.post('/addproducts',imageUploadMiddleware,adminController.createNewProduct);
-router.get('/products/:id',adminController.getUpdateProduct)
-router.post('/products/:id',imageUploadMiddleware,adminController.updateProduct)
+router.get('/products',adminController.getAllProducts); //show product on admin page
+router.get('/products/add',adminController.getNewProduct); // show add product page
+router.post('/addproducts',imageUploadMiddleware,adminController.createNewProduct); //add new product
+router.get('/products/:id',adminController.getUpdateProduct); // show product details
+router.post('/products/:id',imageUploadMiddleware,adminController.updateProduct); // update product details
+router.delete('/products/:id',adminController.deleteProduct) //delete product
 
 module.exports = router;
