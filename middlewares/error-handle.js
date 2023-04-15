cdd485e32a07565babe7a39c37611efb45f640b1
Wichapon V <wichapon@gmail.com>
2023-04-15 21:34:30 +0700

    Update error-handle.js to log and render appropriate errors

--- middlewares/error-handle.js
+++ middlewares/error-handle.js
@@ -2,10 +2,15 @@ function handleErrors(err,req,res,next){
     if (err === 404){
         res.status(404);
         res.render('shared/404');
+        console.log(err);
+        return;
+    }else{
+        res.status(500);
+        res.render('shared/500');
+        console.log(err);
         return;
     }
-    res.status(500);
-    res.render('shared/500');
+    
 }
 
 module.exports = handleErrors;
