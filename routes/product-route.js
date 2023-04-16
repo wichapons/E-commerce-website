191d455026389835d1c9ad04b0f9b6b2e952d96c
Wichapon V <wichapon@gmail.com>
2023-04-16 15:11:19 +0700

    Add route for getting product details

--- routes/product-route.js
+++ routes/product-route.js
@@ -4,4 +4,7 @@ const productController = require('../controllers/products-controller');
 const router = express.Router();
 
 router.get('/products',productController.getAllProducts);
+
+router.get('/products/:id',productController.getProductDetails);
+
 module.exports = router;
