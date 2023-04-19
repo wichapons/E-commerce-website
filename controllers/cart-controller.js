3ba171f5540b4f76e2ad7b54d9138a334516a47a
Wichapon V <wichapon@gmail.com>
2023-04-19 17:06:28 +0700

    Add comments for expaining the code

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -2,20 +2,18 @@ const Product = require('../models/products-model')
 
 async function addCartItem(req,res,next){
     let product;
-
-
     try{
-    product = await Product.findUserID(req.body.productID)
+    product = await Product.findUserID(req.body.productID) //get product data from database
     }catch(err){
         next(err);
         return;
     }
     
-    const cart = res.locals.cart;
-    cart.addItem(product);
-    req.session.cart = cart;
-
-    res.status = 201;
+    const cart = res.locals.cart; //save the local scope from the cart middleware to cart variable 
+    cart.addItem(product); // cart is already get called by cart = new Cart so this will call a method addItem in cart model
+    req.session.cart = cart; // save cart item to session
+    
+    res.status = 201; //return sucess
     res.json({
         message:'Cart updated!',
         totalItem: cart.totalQuantity
--- middlewares/cart.js
+++ middlewares/cart.js
@@ -6,12 +6,13 @@ function initializeCart(req,res,next){
     if(!req.session.cart){
         cart = new Cart();
     }else{
+        //get the session data in cartSession
         const cartSession = req.session.cart;
         cart = new Cart(cartSession.items,
             cartSession.totalQuantity,
             cartSession.totalPrice)
     }
-
+    //save to locals scope will only available for the duration of the current request, and is deleted once the response has been sent.
     res.locals.cart = cart;
     next();
 }
