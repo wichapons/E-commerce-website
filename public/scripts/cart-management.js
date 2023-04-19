eda51f8f6428906d21c0e7d99e916f2b87664665
Wichapon V <wichapon@gmail.com>
2023-04-19 11:30:40 +0700

    Update cart-management.js to add CSRF token and update cart badge element on successful post

--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -1,16 +1,40 @@
 const addToCartButtonElement = document.querySelector('#product-details button');
+const cartBadgeElement = document.querySelector('.nav-items .badge');
+
 
 async function addToCart(){
-    const productID = addToCartButtonElement.dataset.productID;
-    await fetch('/cart/items',{
-        method:'POST',
-        body: JSON.stringify({  // to convert the body.object to JSON format
-            productID:productID
-        }),
-        headers: {
-            'Content-Type':'application/json' // to tell the server that data is sent in json format
-        }
-    });
+    console.log('add to cart has been hit');
+    const productID = addToCartButtonElement.dataset.productid;
+    const csrfToken = addToCartButtonElement.dataset.csrftoken;
+    console.log('csrfToken: '+csrfToken);
+    let response;
+    console.log('add to cart has been hit');
+    try{
+            response = await fetch('/cart/items',{
+            method:'POST',
+            body: JSON.stringify({  // to convert the body.object to JSON format
+                productID:productID,
+                _csrf:csrfToken
+            }),
+            headers: {
+                'Content-Type':'application/json' // to tell the server that data is sent in json format
+            }
+        });
+    } catch(err){
+        console.log(err);
+        next(err);
+    }
+
+    if (!response.ok){
+        alert('something went wrong please try again')
+    }else{
+        const responseData = await response.json(); //get the res.json from cart controller
+        const newTotalQuantity = responseData.totalItem; 
+        console.log('newTotalQuantity: '+newTotalQuantity);
+        cartBadgeElement.textContent = newTotalQuantity; //change number of item in carts 
+    }
+
+
 }
 
-addEventListener('click',addToCart)
+addToCartButtonElement.addEventListener('click',addToCart);
