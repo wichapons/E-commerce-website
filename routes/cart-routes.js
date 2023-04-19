87734a27f2f353e3782e141eb2701c80e7eb16e4
Wichapon V <wichapon@gmail.com>
2023-04-19 20:45:31 +0700

    Add GET route to render cart page

--- routes/cart-routes.js
+++ routes/cart-routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const cartController = require('../controllers/cart-controller')
 
-router.post('/items',cartController.addCartItem)
+router.get('/',cartController.renderCartPage);
+router.post('/items',cartController.addCartItem);
 
 module.exports = router;
