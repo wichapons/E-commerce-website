c34e61325063f4a472c9f04a2ee11fa4b8dad3a3
Wichapon V <wichapon@gmail.com>
2023-04-25 23:13:37 +0700

    Add notes on useful commands for manipulating data types

--- shortnote/useful-commands.js
+++ shortnote/useful-commands.js
@@ -1,1 +1,9 @@
 Object.entries(); // get the keyvalue pair of the object
+variable.toFixed() // When the calculation of floating number occur use toFixed to solve the decimal issue 
+//covert number
+parseFloat 
+parseInt // if item.quantity is "3.14", parseInt(item.quantity) will return 3.
++variable
+number(variable)
+
+
