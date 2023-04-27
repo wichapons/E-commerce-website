9127b5aa07b7ae8928730b9a9bd6d97679161f0e
Wichapon V <wichapon@gmail.com>
2023-04-27 23:48:03 +0700

    Add routes for orders and corresponding controller methods

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -3,12 +3,15 @@ const adminController = require('../controllers/admin-controller');
 const imageUploadMiddleware = require('../middlewares/file-upload')
 
 const router = express.Router();
-
+// products
 router.get('/products',adminController.getAllProducts); //show product on admin page
 router.get('/products/add',adminController.getNewProduct); // show add product page
 router.post('/addproducts',imageUploadMiddleware,adminController.createNewProduct); //add new product
 router.get('/products/:id',adminController.getUpdateProduct); // show product details
 router.post('/products/:id',imageUploadMiddleware,adminController.updateProduct); // update product details
 router.delete('/products/:id',adminController.deleteProduct) //delete product
+// orders
+router.get('/orders', adminController.getOrders);
+router.patch('/orders/:id', adminController.updateOrder);
 
 module.exports = router;
