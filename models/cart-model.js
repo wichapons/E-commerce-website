57f42d68df8c9f6396501c3caaa4c1734a9dfcb0
Wichapon V <wichapon@gmail.com>
2023-04-27 23:46:48 +0700

    Update cart-model to reflect latest product prices and recalculate cart totals

--- models/cart-model.js
+++ models/cart-model.js
@@ -1,3 +1,5 @@
+const Product = require('./products-model');
+
 class Cart {
     constructor(items=[],totalQuantity = 0, totalPrice=0){ //if no value on the cart before use the default value
         this.items = items;
@@ -50,6 +52,46 @@ class Cart {
             }
         }
     }
+
+    async updatePrices() {
+        const productIds = this.items.map(function (item) {
+          return item.product.id;
+        });
+        const products = await Product.findMultiple(productIds);
+        const deletableCartItemProductIds = [];
+
+        //find product id in the cart 
+        for (const cartItem of this.items) {
+          const product = products.find(function (prod) { 
+            return prod.id === cartItem.product.id;
+          });
+          if (!product) {
+            // product was deleted!
+            // "schedule" for removal from cart
+            deletableCartItemProductIds.push(cartItem.product.id);
+            continue; //continue for loop without exucute the code below
+          }
+          // product was not deleted
+          // set product data and total price to latest price from database
+          cartItem.product = product;
+          cartItem.totalPrice = cartItem.quantity * cartItem.product.price;
+        }
+
+        if (deletableCartItemProductIds.length > 0) { 
+          this.items = this.items.filter(function (item) {
+            return deletableCartItemProductIds.indexOf(item.product.id) < 0;
+          });
+        }
+    
+        // re-calculate cart totals
+        this.totalQuantity = 0;
+        this.totalPrice = 0;
+    
+        for (const item of this.items) {
+          this.totalQuantity = this.totalQuantity + item.quantity;
+          this.totalPrice = this.totalPrice + item.totalPrice;
+        }
+      }
 
 }
 
