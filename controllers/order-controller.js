6fb9a0478f4e9da69d1eb1694324d90fb8ade9f7
Wichapon V <wichapon@gmail.com>
2023-05-03 23:26:42 +0700

    Add Stripe payment implementation

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -1,6 +1,11 @@
-const e = require('express');
+// import Model
 const Order = require('../models/orders-model');
 const User = require('../models/user-model');
+
+//import .env variables
+require('dotenv').config();
+//import stripe payment 
+const stripe = require('stripe')(process.env.stripeApiKey);
 
 async function getOrderPage(req,res,next){
     try {
@@ -33,13 +38,45 @@ async function addOrder(req,res,next){
         return;
     }
 
-    req.session.cart = null; //clear items on the cart
+    //make a payment on stripe
+    try{
+        const session = await stripe.checkout.sessions.create({
+            line_items: cart.items.map((item)=>{ //convert item cart into arrays with the format of stripe
+                return  {
+                    price_data: {
+                        currency:'thb',
+                        product_data:{
+                            name:item.product.title,
+                        },
+                        unit_amount: parseFloat(item.product.price).toFixed(2)*100
+                    },
+                    quantity: item.quantity
+                  }
+            }),
+            mode: 'payment',
+            success_url: `http://localhost:3000/orders/success`,
+            cancel_url: `http://localhost:3000/orders/failure`,
+          });
+          res.redirect(303, session.url);
+          return;
+    }catch(error){
+        next(error);
+        return;
+    }
+}
 
-    res.redirect('/orders'); //after save completed redirect to /orders
-    
+function getSuccessPage(req,res){
+    res.render('customer/orders/success');
+    req.session.cart = null; //clear items on the cart
+}
+
+function getFailurePage(req,res){
+    res.render('customer/orders/failure');
 }
 
 module.exports = {
     addOrder:addOrder,
-    getOrderPage:getOrderPage
+    getOrderPage:getOrderPage,
+    getSuccessPage,
+    getFailurePage
 }
