83cd2f02a39ce9f8f4da6e88e0a4e35a63d583c5
Wichapon V <wichapon@gmail.com>
2023-05-04 16:14:28 +0700

    Update database connection for local and production environment

--- database/database.js
+++ database/database.js
@@ -1,10 +1,19 @@
-const mongodb = require('mongodb');
+require('dotenv').config();
 
+const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let database;
+let mongodbURL;
+
+if(process.env.SERVER_STATUS === 'prod'){
+    mongodbURL = process.env.MONGODB_API_KEY
+}else{
+    mongodbURL = 'mongodb://127.0.0.1:27017'
+}
+
 async function connectDb(){
-    const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
+    const client = await MongoClient.connect(mongodbURL);
     database = client.db('Topazio-Online-Shop');
 }
 
